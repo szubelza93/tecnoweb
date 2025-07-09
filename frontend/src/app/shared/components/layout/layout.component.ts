3b7cad3282a24353b6c13a9cf02b1a2f883430af
szubelza93 <szubelza93@gmail.com>
2025-07-09 08:48:00 -0400

    menu amburguesa

--- frontend/src/app/shared/components/layout/layout.component.ts
+++ frontend/src/app/shared/components/layout/layout.component.ts
@@ -3,14 +3,19 @@ import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, SidebarComponent, FooterComponent, RouterOutlet, NgIf],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export default class LayoutComponent {
+  sidebarVisible = true;
 
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
 }
